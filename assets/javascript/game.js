362d8f92079b1bf8c6ec0634f4c771926423d52f
Dafydd <dafyddzephyr@gmail.com>
2018-11-14 18:18:11 -0800

    compares properly now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,12 +4,17 @@ var losses = 0;
 
 //this var holds the value for each button
 var crystalNum = [];
+
+//displays your number and eggman's number
 var eggmanThink = $("#eggNum");
 var sonicNum = $("#yourNum");
-// var totalNum = +totalNum + +pastNum;
 
 //this var holds the sum of every number
 var totalNum = [];
+var compareNum = 0;
+
+//eggman random number
+var eggmanNum = Math.floor(Math.random() * 120) + 19;
 
 //generates 4 random numbers for the 4 buttons
 while (crystalNum.length < 4) {
@@ -17,41 +22,71 @@ while (crystalNum.length < 4) {
     if (crystalNum.indexOf(randomNum) === -1) crystalNum.push(randomNum);
 }
 
+//functions to reduce and add array
 function addTotal(total, num) {
     return total + num;
 }
-
 function updateTotal(e) {
     document.getElementById("yourNum").innerHTML = totalNum.reduce(addTotal);
 }
 
-var eggmanNum = Math.floor(Math.random() * 120) + 19;
+//decides if the game is won yet
+function gameState() {
+    if (compareNum === eggmanNum) {
+        console.log("win");
+    }
+    else if (compareNum > eggmanNum) {
+        console.log("lose");
+    }
+    else {
+        console.log("not yet");
+    }
+}
 
 var lockGame = false;
 
 $(document).ready(function () {
+
     eggmanThink.append(eggmanNum);
-    console.log(eggmanNum);
-    console.log(crystalNum);
-    console.log(totalNum);
-    if (parseInt(totalNum) === parseInt(eggmanNum)) {
-        console.log("win");
-    }
+
+    $("#wins").append(wins);
+    $("#losses").append(losses);
+
+    // console.log("eggman num: " + eggmanNum);
+    console.log("emeralds: " + crystalNum);
+    console.log("current array" + totalNum);
+
     $(".emerald1").on("click", function () {
         totalNum.push(crystalNum[0]);
+        compareNum += crystalNum[0];
         updateTotal();
+        console.log(compareNum);
+        gameState();
     })
+
     $(".emerald2").on("click", function () {
         totalNum.push(crystalNum[1]);
+        compareNum += crystalNum[1];
         updateTotal();
+        console.log(compareNum);
+        gameState();
     })
+
     $(".emerald3").on("click", function () {
         totalNum.push(crystalNum[2]);
+        compareNum += crystalNum[2];
         updateTotal();
+        console.log(compareNum);
+        gameState();
     })
+
     $(".emerald4").on("click", function () {
         totalNum.push(crystalNum[3]);
+        compareNum += crystalNum[3];
         updateTotal();
+        console.log(compareNum);
+        gameState();
     })
-       
+
+
 })
